1a0ad9f0c88750cbcbd8fd918ce683737f9b9180
Timothy Wilder <tk100794@gmail.com>
2017-03-09 12:19:39 -0500

    Hopefully working code for balancing. Needs to be assigned correct pins for all peripherals and needs to be calibrated.

--- cs503-glassesbot.ino
+++ cs503-glassesbot.ino
@@ -1,24 +1,240 @@
 
+#include "Wire.h"                 
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#define MPU_INT 2//0
+#define PWM_L 10
+#define PWM_R 11
+#define DIR_L1 6
+#define DIR_L2 12
+#define DIR_R1 8
+#define DIR_R2 9
+#define SPD_INT_L 3//1
+#define SPD_PUL_L 4
+#define SPD_INT_R 4//7
+#define SPD_PUL_R 5
+
 float translate_velocity = 0.0f;
 float turn_velocity = 0.0f;
 
-float K = 60.0f;
-float B = 60.0f;
+float gravity_offset = 0.2f;
+float K = -60.0f;
+float B = -60.0f;
 float angle_ref = 0.0f;
 
-void setup() {
+MPU6050 mpu;
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+int16_t gyro[3];        // [x, y, z]            gyro vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
+{
+  mpuInterrupt = true;
+}
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    init_IO();
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
+    // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(2, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(2), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+  
 }
 
 void loop() {
-
-  // Read gyro
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  while (!mpuInterrupt && fifoCount < packetSize) {
+    
+  }
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
   
-  float deltaPWM = K*(angle - angle_ref) + B*(angle_rate);
-  static float PWMout += deltaPWM;
-  PWMout += translate_velocity;
-  static float PWMout_left -= turn_velocity;
-  static float PWMout_right += turn_velocity;
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+  
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+  {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  }
+  else if (mpuIntStatus & 0x02)
+  {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+    //Get sensor data
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGyro(gyro, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    
+    // angle and angular rate unit: radian
+    float angle_err = ypr[1] + angle_ref;               // angle_ref is center of gravity offset
+    double angular_rate = -((double)gyro[1]/131.0);     // converted to radians
 
-  // Control motor
+    displayYPR();
+    
+    float deltaPWM = K*angle_err + B*angular_rate + translate_velocity;
+    static int pwm_left;
+    pwm_left += deltaPWM + turn_velocity;
+    static int pwm_right;
+    pwm_right += deltaPWM - turn_velocity;
+
+    Serial.println(pwm_left);
+
+    // Control motor
+    pwm_out(pwm_left, pwm_right);
+  }
+}
+
+void displayYPR() {
+  // display Euler angles in degrees
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  Serial.print("ypr\t");
+  Serial.print(ypr[0] * 180/M_PI);
+  Serial.print("\t");
+  Serial.print(ypr[1] * 180/M_PI);
+  Serial.print("\t");
+  Serial.println(ypr[2] * 180/M_PI);
+}
+
+
+void pwm_out(int l_val,int r_val)
+{
+  if (l_val<0)
+  {
+    digitalWrite(DIR_L1, HIGH);
+    digitalWrite(DIR_L2, LOW);
+    l_val=-l_val;
+  }
+  else
+  {
+    digitalWrite(DIR_L1, LOW);
+    digitalWrite(DIR_L2, HIGH);
+  }
+  
+  if (r_val<0)
+  {
+    digitalWrite(DIR_R1, HIGH);
+    digitalWrite(DIR_R2, LOW);
+    r_val=-r_val;
+  }
+  else
+  {
+    digitalWrite(DIR_R1, LOW);
+    digitalWrite(DIR_R2, HIGH);
+  }
+  l_val=l_val+5;
+  r_val=r_val+30;
+  analogWrite(PWM_L, l_val>255? 255:l_val);
+  analogWrite(PWM_R, r_val>255? 255:r_val);
+}
+
+
+void init_IO()
+{
+  // configure I/O
+  pinMode(SPD_PUL_L, INPUT);//
+  pinMode(SPD_PUL_R, INPUT);//
+  pinMode(PWM_L, OUTPUT);//
+  pinMode(PWM_R, OUTPUT);//
+  pinMode(DIR_L1, OUTPUT);//
+  pinMode(DIR_L2, OUTPUT);
+  pinMode(DIR_R1, OUTPUT);//
+  pinMode(DIR_R2, OUTPUT);
+  pinMode(A0, INPUT);digitalWrite(A0, HIGH);
+  pinMode(A1, INPUT);digitalWrite(A1, HIGH);
+  pinMode(A2, INPUT);digitalWrite(A2, HIGH);
+  pinMode(A3, INPUT);digitalWrite(A3, HIGH);
 }
