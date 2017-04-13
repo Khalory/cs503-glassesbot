72cc160149fd0aca3e301a0495a4e981aba60661
Timothy Wilder <tk100794@gmail.com>
2017-04-13 14:03:23 -0400

    A few fixes and tests

--- cs503-glassesbot.ino
+++ cs503-glassesbot.ino
@@ -18,7 +18,7 @@
 
 // Left encoder
 #define IR_1 2
-#define IR_2 4
+#define IR_2 6
 // Right encoder
 #define IR_3 3
 #define IR_4 5
@@ -32,28 +32,49 @@ volatile long rightEncoder = 0;
 // Tracks the number of steps since the last world position update
 volatile int leftSteps = 0;
 volatile int rightSteps = 0;
-const int stepsPerUpdate = 3;
+const int stepsPerUpdate = 1;
 
-const float botRadius = 1.375f;
-const float stepDistance = 2.0f * botRadius * PI / 64.0f;
+// MOTION CONTROLLER CODE (JUST THE ONE LINE BELOW)
+const float max_phi = (10.0f / 180.0f) * PI; // Max angle offset of robot (5 degrees currently)
+
+const float botRadius = 4.125f;
+const float wheelRadius = 1.39f;
+const float stepDistance = 2.0f * wheelRadius * PI / 16.0f;
 
 // World Coordinates
 float worldX = 0.0f;  
 float worldY = 0.0f;
 float worldTheta = 0.0f;
 
+// MOTION CONTROLLER CODE (BELOW)
+// Previous World Coordinates
+float prevWorldX = 0.0f;  
+float prevWorldY = 0.0f;
+float prevWorldTheta = 0.0f;
+float prevWorldTime = millis();
+
+// Current Velocity
+float xVelocity = 0.0f;
+float yVelocity = 0.0f;
+float thetaVelocity = 0.0f;
+// Goal location of the current goal we are trying to reach
+float goalLocationX = 0.001;
+float goalLocationY = 0;
+float goalLocationTheta = atan2(goalLocationY, goalLocationX); //
+
+
 float translate_velocity = 0.0f;
 float turn_velocity = 0.0f;
 
-// 28, 22 work pretty well
-// 45, 26 works best so far
-// 55, 38 was decent but wobly
-// 55, 25 is pretty pretty
 float K = 4.5f;
 float B = 0.13f;
+// TODO FIND CORRECT K's and B's, MOTION CONTROLLER CODE 
+float K_t = 1.0f;
+float B_t = 0.1f;
+float K_r = 0.0f;
+float B_r = 0.0f;
 float angle_ref = 0.02f;
 float angular_rate_ref = 0.0f;
-float wheel_rate_correction = 1.0f;//1.065f; // This gets multiplied by the wheel with more tilt so that we move straight
 
 float pwm_left;
 float pwm_right;
@@ -86,7 +107,7 @@ void setup() {
     Serial.begin(115200);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
-    // initialize device
+    // initialize devices
     mpu.initialize();
     md.init();
     init_IR();
@@ -95,9 +116,6 @@ void setup() {
 }
 
 void loop() {
-  if (shouldUpdateCoords())
-    updateCoords();
-  
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
   getPitch(&pitch, &ax, &az);
   //Serial.print(ax);
@@ -106,8 +124,47 @@ void loop() {
   //angle and angular rate unit: radian
   double angle_err = pitch - angle_ref;               // angle_ref is center of gravity offset
   double angular_rate = PI*((double)gy)/180 - angular_rate_ref;     // converted to radians
+
+  // MOTION CONTROLLER CODE (BELOW)
+  // Translate Controller
+  double distance_to_goal = pow((pow(goalLocationX - worldX, 2) + pow(goalLocationY - worldY, 2)), .5);
+  double distance_direction = atan2(goalLocationX - worldX, goalLocationY - worldY) + worldTheta;
+  if (distance_direction > PI || distance_direction < -PI)
+    distance_to_goal *= -1;
+  printWorldCoords();
+  double velocity = (pow((pow(xVelocity, 2) + pow(yVelocity, 2)), .5));
+  double velocity_direction = atan2(xVelocity, yVelocity) + worldTheta;
+  if (!(velocity_direction > PI || velocity_direction < -PI) ^ (distance_direction > PI || distance_direction < -PI))
+    velocity *= -1;
+  Serial.print(distance_to_goal);
+  Serial.print(", ");
+  Serial.print(velocity);
+  Serial.print(" :: ");
+  double delta_phi = K_t * distance_to_goal - B_t * velocity;
+  Serial.print(delta_phi);
+  Serial.print(" -> ");
+  if (delta_phi > max_phi) {
+    delta_phi = max_phi;
+  }
+  if (delta_phi < -max_phi) {
+    delta_phi = -max_phi;
+  }
+  angle_err += delta_phi;
+  Serial.println(delta_phi);
+  // Balance Controller
+  float deltaPWM = -K * angle_err - B * angular_rate;
+  // Rotate Controller
+//  float deltaPWMR = K_r * (goalLocationTheta - worldTheta) - B_r * (thetaVelocity);
+//  if (goalLocationTheta - worldTheta >= 0) {
+//      pwm_left -= deltaPWMR;
+//      pwm_right += deltaPWMR;
+//  } else {
+//      pwm_left += deltaPWMR;
+//      pwm_right -= deltaPWMR;
+//  }
+  // MOTION CONTROLLER CODE (ABOVE)
   
-  float deltaPWM = -K*angle_err - B*angular_rate;
+  //float deltaPWM = -K*angle_err - B*angular_rate;
   //printAngleErrorAndSpeed(&angle_err, &angular_rate);
   //printDeltaPWMComponents(&deltaPWM, &K, &pitch, &angle_ref, &B, &gy);
   //printDeltaPWMEquation(&deltaPWM, &K, &pitch, &angle_ref, &B, &gy);
@@ -128,19 +185,24 @@ void loop() {
 
   float leftOut = pwm_left;
   float rightOut = pwm_left;
-  if (leftOut < 0)
-    leftOut -= 20;
-  if (rightOut < 0)
-    rightOut -= 20;
-  if (leftOut > 0)
-    leftOut += 20;
-  if (rightOut > 0)
-    rightOut += 20;
+//  if (leftOut < 0)
+//    leftOut -= 20;
+//  if (rightOut < 0)
+//    rightOut -= 20;
+//  if (leftOut > 0)
+//    leftOut += 20;
+//  if (rightOut > 0)
+//    rightOut += 20;
 
   //Control motor
-  //md.setSpeeds(leftOut, rightOut);
-  
-  printPIND();
+  md.setSpeeds(leftOut, rightOut);
+
+  //Serial.println(analogRead(A0));
+  //analog_encoder_two();
+  //Serial.print(analogRead(A0));
+  //Serial.print(", ");
+//  Serial.println(analogRead(A1));
+  //printPIND();
 }
 
 double pitchFlow = 1.0d;
@@ -148,9 +210,7 @@ double integratedPitch = 0.0d;
 void getPitch(double *pitch, int16_t *ax, int16_t *az) {
   // Just using atan2 gives us an angle where a balanced position reads as either PI or -PI
   // Inverted so it's in the same direction as the gyro
-  *pitch = -atan2(*ax, *az) - PI;
-  if (*pitch < -PI)
-    *pitch += 2*PI;
+  *pitch = -atan2(*ax, *az);
   *pitch = (1.0d - pitchFlow) * integratedPitch + pitchFlow * (*pitch);
   integratedPitch = *pitch;
 }
@@ -162,8 +222,12 @@ void init_IR()
   pinMode(IR_2, INPUT);
   pinMode(IR_3, INPUT);
   pinMode(IR_4, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
   attachInterrupt(IR_INTERRUPT_1, encoder_one, CHANGE);
   attachInterrupt(IR_INTERRUPT_2, encoder_two, CHANGE);
+  encoder_one(); // Initialize the encoders with the starting digital pins
+  encoder_two();
 }
 
 // IR interrupts for the quadrature encoders
@@ -177,14 +241,25 @@ void encoder_one()
   
   enc_val = enc_val << 2;
   // PIND reads the first 8 digital pins (0-7)
-  uint8_t pins = (PIND & 0b00010100) >> 2; // Interrupt on 2, regular on 4 (interrupts available only on 2,3)
-  pins = (pins & 0b1) | ((pins >> 1) & 0b10);
+  uint8_t pins = (PIND & 0b01000100) >> 2;
+  pins = (pins & 0b1) | ((pins >> 3) & 0b10);
   enc_val = enc_val | pins;
 
-  leftEncoder += lookup_table[enc_val & 0b1111];
-  leftSteps += lookup_table[enc_val & 0b1111];
-  //Serial.print("Left position: ");
-  //Serial.println(leftEncoder);
+  if (enc_val > 3) {
+    leftEncoder -= lookup_table[enc_val & 0b1111];
+    leftSteps -= lookup_table[enc_val & 0b1111]; 
+  }
+  else
+    enc_val = enc_val | 0b10000;
+  
+  if (shouldUpdateCoords())
+    updateCoords();
+
+  
+//  Serial.print("Left position: ");
+//  Serial.print(leftEncoder);
+//  Serial.print("    Left step: ");
+//  Serial.println(leftSteps);
 }
 
 void encoder_two()
@@ -201,52 +276,76 @@ void encoder_two()
   pins = (pins & 0b1) | ((pins >> 1) & 0b10);
   enc_val = enc_val | pins;
 
-  rightEncoder += lookup_table[enc_val & 0b1111];
-  rightSteps += lookup_table[enc_val & 0b1111];
-  Serial.print("Right position: ");
-  Serial.println(rightEncoder);
+  if (enc_val > 3) {
+    rightEncoder -= lookup_table[enc_val & 0b1111];
+    rightSteps -= lookup_table[enc_val & 0b1111]; 
+  }
+  else
+    enc_val = enc_val | 0b10000;
+  
+  if (shouldUpdateCoords())
+    updateCoords();
+    
+  //Serial.print("Right position: ");
+  //Serial.println(rightEncoder);
 }
 
 bool shouldUpdateCoords() {
-  return leftSteps + rightSteps >= stepsPerUpdate;
+  return abs(leftSteps) + abs(rightSteps) >= stepsPerUpdate;
 }
 
 void updateCoords() {
-  Serial.print(leftEncoder);
-  Serial.print(" : ");
-  Serial.println(rightEncoder);
-  printWorldCoords();
   float dsl = leftSteps * stepDistance;
-  leftSteps = 0; // Might have concurrency issues with the interrupt adding to these variables
+  leftSteps = 0;
   float dsr = rightSteps * stepDistance;
   rightSteps = 0;
   float dsavg = (dsl + dsr) / 2;
   float dTheta = (dsl - dsavg)/botRadius;
-
+//  Serial.print(leftEncoder);
+//  Serial.print(" : ");
+//  Serial.println(rightEncoder);
+//  Serial.print("Left step: ");
+//  Serial.print(leftSteps);
+//  Serial.print("   dsl: ");
+//  Serial.print(dsl);
+//  Serial.print("   dsr: ");
+//  Serial.print(dsr);
+//  Serial.print("   dsavg: ");
+//  Serial.print(dsavg);
+//  Serial.print("   dthetha: ");
+//  Serial.println(dTheta);
+  //printWorldCoords();
   worldTheta += dTheta;
-  if (worldTheta > 2*PI)
+  while(worldTheta > PI)
     worldTheta -= 2*PI;
-  if (worldTheta < 0)
+  while(worldTheta < -PI)
     worldTheta += 2*PI;
   worldX += dsavg * cos(worldTheta); // Or [worldTheta - (dTheta/2)]
-  worldY += dsavg * sin(worldTheta);
+  worldY += dsavg * sin(worldTheta); 
+//  printWorldCoords();
+  
+   // MOTION CONTROLLER CODE
+  double delta_t = (millis() - prevWorldTime) * 1000; // find time that has past and convert to seconds
+  xVelocity = (worldX - prevWorldX) / delta_t;
+  yVelocity = (worldY - prevWorldY) / delta_t;
+  thetaVelocity = (worldTheta - prevWorldTheta) / delta_t;
+  prevWorldX = worldX;
+  prevWorldY = worldY;
+  prevWorldTheta = worldTheta;
+  prevWorldTime = millis();
 }
 
 void init_IO()
 {
   // configure I/O
-  pinMode(SPD_PUL_L, INPUT);//
-  pinMode(SPD_PUL_R, INPUT);//
-  pinMode(PWM_L, OUTPUT);//
-  pinMode(PWM_R, OUTPUT);//
-  pinMode(DIR_L1, OUTPUT);//
+  pinMode(SPD_PUL_L, INPUT);
+  pinMode(SPD_PUL_R, INPUT);
+  pinMode(PWM_L, OUTPUT);
+  pinMode(PWM_R, OUTPUT);
+  pinMode(DIR_L1, OUTPUT);
   pinMode(DIR_L2, OUTPUT);
-  pinMode(DIR_R1, OUTPUT);//
+  pinMode(DIR_R1, OUTPUT);
   pinMode(DIR_R2, OUTPUT);
-  pinMode(A0, INPUT);digitalWrite(A0, HIGH);
-  pinMode(A1, INPUT);digitalWrite(A1, HIGH);
-  pinMode(A2, INPUT);digitalWrite(A2, HIGH);
-  pinMode(A3, INPUT);digitalWrite(A3, HIGH);
 }
 
 void printDeltaPWMComponents(float *deltaPWM, float *K, double *pitch, float *angle_ref, float *B, int16_t *gy)
@@ -305,4 +404,3 @@ void printPIND()
   Serial.print((PIND & 0b00000001));
   Serial.println();
 }
-
